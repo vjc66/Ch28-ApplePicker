4f3762b7fc69bf384afa60eaacfb738bf955619e
vjc66 <55262819+vjc66@users.noreply.github.com>
2019-09-25 16:35:11 -0400

    Trees, Apples, Baskets, Color, and some Coding

---
+++ Assets/Scenes/Scripts/AppleTree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{  [Header("Set in Inspector")]
+   
+   public GameObject applePrefab;
+
+public float speed = 1f;
+
+public float leftAndRightEdge = 10f;
+
+public float chanceToChangeDirections = 0.1f;
+
+public float secondsBetweenAppleDrops = 1f;
+
+void Start()
+{
+    // Dropping apples every second
+}
+
+void Update()
+{
+    // Basic Movement
+    // Changing Direction
+}
+}
